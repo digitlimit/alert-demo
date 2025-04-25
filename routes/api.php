cbb43fcaf91670760f7f20f7e4f249677f9d27c3
Emeka Mbah <frankemeks77@yahoo.com>
2025-04-25 06:39:37 +0100

    feat: fix id

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/id', function (Request $request) {
+    return $request->user()->id;
+})->middleware('auth:sanctum');
